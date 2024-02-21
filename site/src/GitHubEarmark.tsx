e774b5f69ffcc1a206ffda6d8343d6b24fb06b03
Bell, Ryan <ryan.bell62101@gmail.com>
2024-02-21 00:55:39 -0600

    fixed typing error

--- site/src/GitHubEarmark.tsx
+++ site/src/GitHubEarmark.tsx
@@ -2,12 +2,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 import 'styles/GitHubEarmark.css'
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 export default function GitHubEarmark() {
     return (
         <a href="https://github.com/raian621/rand-um"> 
             <div className="github-earmark">
-                <FontAwesomeIcon icon={faGithub} color="white"/>
+                <FontAwesomeIcon icon={faGithub as IconProp} color="white"/>
             </div>
         </a>
     )
